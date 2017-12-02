342d451d40bbcba9cfc122898b20d468e0bb695b
Allen <love525150@163.com>
2017-12-02 21:03:54 +0800

    加入redis连接

--- src/main/java/org/allen/controller/MonitorController.java
+++ src/main/java/org/allen/controller/MonitorController.java
@@ -1,14 +1,25 @@
 package org.allen.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("monitor")
 public class MonitorController {
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
 
     @RequestMapping("ok")
     public String ok() {
         return "ok";
+    }
+
+    @RequestMapping("redis")
+    public String redis() {
+        ValueOperations<String, String> operations = stringRedisTemplate.opsForValue();
+        return operations.get("key");
     }
 }
